0186546beaffb95dc21b3e879e310b0c35de3e39
Blessing-AkP <blessing.as262@gmail.com>
2023-09-18 13:05:46 +0100

    enabled cors policy and other security dependencies

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = express();
 require('dotenv').config();
 require('express-async-errors');
 
+//Extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 // DB module
 const connectDB = require('./db/connect'); 
 
@@ -21,14 +27,17 @@ const  jobsRouter = require('./routes/jobs');
 const auth = require('./middleware/authentication')
 
 //Middleware
-app.use(express.json())
-
-
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}))
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // Routes
-app.get('/', (req, res)=>{
-  res.send('Jobs API')
-})
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', auth, jobsRouter);
 
